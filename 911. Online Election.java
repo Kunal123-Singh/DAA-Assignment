4e34ed5c7abed07625ad7c4eb34c77589ab910f3
Kunal123-Singh <ks1871941@gmail.com>
2026-08-05 23:50:33 +0530

    Create 911. Online Election.java

---
+++ 911. Online Election.java
@@ -0,0 +1,21 @@
+class TopVotedCandidate {
+    TreeMap<Integer, Integer> timedWinner;
+    int lead;
+    public TopVotedCandidate(int[] persons, int[] times) {
+        int n = times.length;
+        lead = -1;
+        timedWinner = new TreeMap();
+        Map<Integer, Integer> votes = new HashMap();
+        for(int i=0; i<n; i++){
+            votes.put(persons[i], votes.getOrDefault(persons[i], 0)+1);
+            if(i==0 || votes.get(persons[i])>=votes.get(lead)){
+               lead = persons[i];
+            }
+            timedWinner.put(times[i], lead);  
+        }
+    }
+    
+    public int q(int t) {
+        return timedWinner.floorEntry(t).getValue();
+    }
+}
